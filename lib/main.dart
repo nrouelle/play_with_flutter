d87ea5a0de05bf7958d715703a273a3f669bb78e
Nadege ROUELLE <nadege.rouelle.ext@careside.care>
2025-04-28 14:16:58 +0200

    add title field and form validation

--- lib/main.dart
+++ lib/main.dart
@@ -54,12 +54,16 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final _formKey = GlobalKey<FormState>();
 
   void _showSnackBar() {
-    final snackBar = SnackBar(
-      content: const Text('Livre ajouté!'),
-      action: SnackBarAction(label: 'Annuler', onPressed: (){},),);
-    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+    if (_formKey.currentState!.validate()) {
+      final snackBar = SnackBar(
+        content: const Text('Livre ajouté!'),
+        action: SnackBarAction(label: 'Annuler', onPressed: () {}),
+      );
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+    }
   }
 
   @override
@@ -83,34 +87,40 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              'Coucou',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        child: Form(
+          key: _formKey,
+          child: Column(
+            // Column is also a layout widget. It takes a list of children and
+            // arranges them vertically. By default, it sizes itself to fit its
+            // children horizontally, and tries to be as tall as its parent.
+            //
+            // Column has various properties to control how it sizes itself and
+            // how it positions its children. Here we use mainAxisAlignment to
+            // center the children vertically; the main axis here is the vertical
+            // axis because Columns are vertical (the cross axis would be
+            // horizontal).
+            //
+            // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+            // action in the IDE, or press "p" in the console), to see the
+            // wireframe for each widget.
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text('Titre du livre :'),
+              TextFormField(
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return ('Merci d\'indiquer le titre !');
+                  }
+                  return null;
+                },
+              ),
+              ElevatedButton(
+                onPressed: _showSnackBar,
+                child: const Text('Ajouter'),
+              ),
+            ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _showSnackBar,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
